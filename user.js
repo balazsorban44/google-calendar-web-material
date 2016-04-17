dc16ff15a3d0884b97a9fd32f6740235e1d5987c
Balázs Orbán <info@balazsorban.com>
2016-04-17 17:37:03 +0200

    test

--- user.js
+++ user.js
@@ -1,12 +1,15 @@
-// ==UserScript==
-// @name          Google Calendar Web Material
-// @description   Google Calendar Web Material event flairs
-// @match         https://calendar.google.com/*
-// @version       1.0
-// ==/UserScript==
+  // ==UserScript==
+  // @author        info@balazsorban.com
+  // @name          Google Calendar Web Material
+  // @description   Google Calendar Web Material event flairs
+  // @match         https://calendar.google.com/*
+  // @version       1.0
+  // ==/UserScript==
 
-for (i = 0; i < document.querySelectorAll('.chip dd').length; i++) {
-    if (document.querySelectorAll(".chip dd")[i].innerHTML.indexOf('Work') != -1){
-      document.querySelectorAll(".chip dd")[i].style.backgroundImage = "url('https://raw.githubusercontent.com/balazsorban44/google-calendar-web-material/master/images/event_flairs/gym.png')";
-    }
-}
+  window.onload=function event_flairs(){
+  for (i = 0; i < document.querySelectorAll('.chip dd').length; i++) {
+      if (document.querySelectorAll(".chip dd")[i].innerHTML.indexOf('Work') != -1){
+        document.querySelectorAll(".chip dd")[i].style.backgroundImage = "url('https://raw.githubusercontent.com/balazsorban44/google-calendar-web-material/master/images/event_flairs/gym.png')";
+      }
+  }
+  }
