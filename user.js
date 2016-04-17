440c5e34de39a9da2103358863caa1a990cb8303
Balázs Orbán <info@balazsorban.com>
2016-04-17 16:40:29 +0200

    tests

---
+++ user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name          Google Calendar Web Material
+// @description   Google Calendar Web Material event flairs
+// @match         https://calendar.google.com/*
+// @version       1.0
+// ==/UserScript==
+
+for (i = 0; i < 1000; i++) {
+    if (document.querySelectorAll(".chip dd")[i].innerHTML.indexOf('Work') != -1){
+      document.querySelectorAll(".chip dd")[i].style.backgroundImage = "url('https://raw.githubusercontent.com/balazsorban44/google-calendar-web-material/master/images/event_flairs/gym.png')";
+    }
+}
