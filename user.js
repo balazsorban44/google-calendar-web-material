63f08a474606e3fa0be44fed287fbdc242bfe7b7
Balázs Orbán <info@balazsorban.com>
2016-04-17 21:34:10 +0200

    test

--- user.js
+++ user.js
@@ -10,10 +10,30 @@
 
 var select=document.querySelectorAll('.chip dd');
   for (i = 0; i < select.length; i++) {
-      var gym = ["gym", "Work", "Work out"],
+    var dinner = ["dinner", "Dinner"];
+    var gym = ["Gym", "Work", "Work out"];
+    var code = ["code", "coding", "programming"];
+    var photo = ["Photography", "Photo", "photos", "image"];
+    var clean = ["vacuum clean", "Clean"];
     compareFunction = function(e) { return select[i].innerHTML.indexOf(e) !== -1; };   
 if (gym.filter(compareFunction).length)
      {
         select[i].style.backgroundImage = "url('https://goo.gl/6uktJo')";
+      }
+else if (dinner.filter(compareFunction).length)
+     {
+        select[i].style.backgroundImage = "url('https://goo.gl/O14CLC')";
+      }
+else if (code.filter(compareFunction).length)
+     {
+        select[i].style.backgroundImage = "url('https://goo.gl/uIiBCo')";
+      }
+else if (photo.filter(compareFunction).length)
+     {
+        select[i].style.backgroundImage = "url('https://goo.gl/7LjvkO')";
+      }
+else if (clean.filter(compareFunction).length)
+     {
+        select[i].style.backgroundImage = "url('https://goo.gl/2A76Xn')";
       }
   }
