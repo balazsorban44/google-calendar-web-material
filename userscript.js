83aab1d51a8d7df077c6c0e601b20e0b648b32c7
Balázs Orbán <info@balazsorban.com>
2016-04-17 15:14:19 +0200

    v1.0
    Testing

--- userscript.js
+++ userscript.js
@@ -1,5 +1,14 @@
 // ==UserScript==
 // @name          Google Calendar Web Material
+// @description   Google Calendar Web Material event flairs
 // @match         https://calendar.google.com/*
 // @version       1.0
 // ==/UserScript==
+
+var bg= document.getElementsByClassName("bubblemain");
+var title= document.getElementsByClassName("ui-sch-schmedit")[0].innerHTML;
+if(  title.indexOf('gig') != -1|| title.indexOf('concert') != -1 ){
+    bg[0].style.backgroundImage = "url('https://raw.githubusercontent.com/balazsorban44/google-calendar-web-material/master/images/event_flairs/gig.png')";
+} else if(  title.indexOf('Work') != -1|| title.indexOf('Workout') != -1 ){
+    bg[0].style.backgroundImage = "url('https://raw.githubusercontent.com/balazsorban44/google-calendar-web-material/master/images/event_flairs/gym.png')";
+}
