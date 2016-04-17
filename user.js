af800da34e3ea0a8b1f20983b323fcd768bb24c4
Balázs Orbán <info@balazsorban.com>
2016-04-17 16:56:41 +0200

    test

--- user.js
+++ user.js
@@ -5,7 +5,7 @@
 // @version       1.0
 // ==/UserScript==
 
-for (i = 0; i < 1000; i++) {
+for (i = 0; i < document.querySelectorAll('.chip dd').length; i++) {
     if (document.querySelectorAll(".chip dd")[i].innerHTML.indexOf('Work') != -1){
       document.querySelectorAll(".chip dd")[i].style.backgroundImage = "url('https://raw.githubusercontent.com/balazsorban44/google-calendar-web-material/master/images/event_flairs/gym.png')";
     }
