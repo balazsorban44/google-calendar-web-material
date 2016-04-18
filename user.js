3a7ff61c84418e6d03e9c96c749b17ed9b3a5636
Balázs Orbán <info@balazsorban.com>
2016-04-18 13:00:01 +0200

    Working arrays!

--- user.js
+++ user.js
@@ -1,57 +1,30 @@
   // ==UserScript==
-  // @author        info@balazsorban.com
+  // @author        Balázs Orbán (info@balazsorban.com)
   // @name          Google Calendar Web Material
   // @description   Google Calendar Web Material event flairs
   // @match         https://calendar.google.com/*
   // @version       1.0
-  // @grant GM_addStyle
   // ==/UserScript==
 
-var event_flair = [
-      ["Dinner", "dinner", ["url('https://goo.gl/O14CLC')"]],
-      ["Work", "gym", ["url('https://goo.gl/6uktJo')"]],
-    ];
-var select=document.querySelectorAll('.chip dd');
-  for (i = 0; i < select.length; i++) {
-      if(select[i].innerHTML.indexOf("Fysikk"));{
-      
-    //   compareFunction = function(e) { return select[i].innerHTML.indexOf(e) !== -1; };   
-//if (event_flair[0].filter(compareFunction).length)
-  //   {
-         
-    //    select[i].style.backgroundImage = event_flair[i][1];
-      }}
-      
-      
-    var dinner = ["dinner", "Dinner"];
-    var gym = ["Gym", "Work", "Work out"];
-    var code = ["code", "Fysikk", "programming"];
-    var photo = ["Photography", "Photo", "photos", "image"];
-    var clean = ["vacuum clean", "Clean"];
-    var math = ["Matematikk"];
-    compareFunction = function(e) { return select[i].innerHTML.indexOf(e) !== -1; };   
-if (gym.filter(compareFunction).length)
+  var event_flair =[
+    ["Work out", "Work", "url(https://goo.gl/6uktJo)"],
+    ["Matematikk","Fysikk","url(https://goo.gl/PW8YmH)"],
+    ["code","programming","url(https://goo.gl/uIiBCo)"],
+    ["Run","jog","url(https://goo.gl/H7KTYU)"],
+    ["Clean","wash","url(https://goo.gl/2A76Xn)"],
+    ["Spanish","German","url(https://goo.gl/z1fy1s)"],
+    ["Photography","photo","url(https://goo.gl/7LjvkO)"]
+
+];
+var select=document.querySelectorAll('.chip dd, .cbrdcc');
+   for (i = 0; i < select.length; i++)
+   {
+     compareFunction = function(e) { return select[i].innerText.indexOf(e) !== -1; };
+     for (j = 0; j < event_flair.length; j++){
+     if (event_flair[j].filter(compareFunction).length)
      {
-        select[i].style.backgroundImage = "url('https://goo.gl/6uktJo')";
-      }
-else if (dinner.filter(compareFunction).length)
-     {
-        select[i].style.backgroundImage = "url('https://goo.gl/O14CLC')";
-      }
-else if (code.filter(compareFunction).length)
-     {
-        select[i].style.backgroundImage = "url('https://goo.gl/uIiBCo')";
-      }
-else if (photo.filter(compareFunction).length)
-     {
-        select[i].style.backgroundImage = "url('https://goo.gl/7LjvkO')";
-      }
-else if (clean.filter(compareFunction).length)
-     {
-        select[i].style.backgroundImage = "url('https://goo.gl/2A76Xn')";
-      }
-      else if (math.filter(compareFunction).length)
-     {
-        select[i].style.backgroundImage = "url('https://goo.gl/PW8YmH')";
-      }
-  }
+       select[i].style.backgroundImage = event_flair[j][event_flair[j].length-1];
+       //TESTING//console.log(event_flair[j][0] + " " + event_flair[j][event_flair[j].length-1]);
+     }
+   }
+   };
