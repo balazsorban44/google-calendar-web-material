0767e22e48a6f66f071d250f1f373b6f76d3dd88
Balázs Orbán <info@balazsorban.com>
2016-04-19 19:05:15 +0200

    Everything works!
    First time both userstyle and userscript works together!
    
    Work left:
    
    - Find all the event flairs/ event flair images
    - Upload event flair images to event_flairs folder
    - Make an array for the whole thing and put it into the userscript

--- user.js
+++ user.js
@@ -1,30 +1,42 @@
-  // ==UserScript==
-  // @author        Balázs Orbán (info@balazsorban.com)
-  // @name          Google Calendar Web Material
-  // @description   Google Calendar Web Material event flairs
-  // @match         https://calendar.google.com/*
-  // @version       1.0
-  // ==/UserScript==
+// ==UserScript==
+// @author        Balázs Orbán (info@balazsorban.com)
+// @name          Google Calendar Web Material
+// @description   Google Calendar Web Material event flairs
+// @match         https://calendar.google.com/*
+// @version       1.0
+// ==/UserScript==
 
-  var event_flair =[
-    ["Work out", "Work", "url(https://goo.gl/6uktJo)"],
-    ["Matematikk","Fysikk","url(https://goo.gl/PW8YmH)"],
-    ["code","programming","url(https://goo.gl/uIiBCo)"],
-    ["Run","jog","url(https://goo.gl/H7KTYU)"],
-    ["Clean","wash","url(https://goo.gl/2A76Xn)"],
-    ["Spanish","German","url(https://goo.gl/z1fy1s)"],
-    ["Photography","photo","url(https://goo.gl/7LjvkO)"]
-
+var event_flair = [
+  ["Work out", "Work", "url(https://goo.gl/6uktJo)"],
+  ["Matematikk", "Fysikk", "url(https://goo.gl/PW8YmH)"],
+  ["code", "programming", "url(https://goo.gl/uIiBCo)"],
+  ["Run", "jog", "url(https://goo.gl/H7KTYU)"],
+  ["Clean", "wash", "url(https://goo.gl/2A76Xn)"],
+  ["Spanish", "German", "url(https://goo.gl/z1fy1s)"],
+  ["Photography", "photo", "url(https://goo.gl/7LjvkO)"]
 ];
-var select=document.querySelectorAll('.chip dd, .cbrdcc');
-   for (i = 0; i < select.length; i++)
-   {
-     compareFunction = function(e) { return select[i].innerText.indexOf(e) !== -1; };
-     for (j = 0; j < event_flair.length; j++){
-     if (event_flair[j].filter(compareFunction).length)
-     {
-       select[i].style.backgroundImage = event_flair[j][event_flair[j].length-1];
-       //TESTING//console.log(event_flair[j][0] + " " + event_flair[j][event_flair[j].length-1]);
-     }
-   }
-   };
+
+var target = document.querySelector('#gridcontainer');
+var observer = new MutationObserver(function(mutations) {
+  mutations.forEach(function(mutation) {
+      console.log("changed");
+      var select = document.querySelectorAll('.chip dd, .cbrdcc');
+      for (i = 0; i < select.length; i++) {
+          compareFunction = function(e) {
+              return select[i].innerText.indexOf(e) !== -1;
+          };
+          for (j = 0; j < event_flair.length; j++) {
+              if (event_flair[j].filter(compareFunction).length) {
+                  select[i].style.backgroundImage = event_flair[j][event_flair[j].length - 1];
+                  //TESTING//console.log(event_flair[j][0] + " " + event_flair[j][event_flair[j].length-1]);
+              }
+          }
+      }
+  });
+});
+var config = {
+  attributes: true,
+  childList: true,
+  characterData: true
+};
+observer.observe(target, config);
